11ef9eacdbe8ce43aa86da1932647da8c0c6ec2f
Mark Peng <mark594530@gmail.com>
2023-07-10 03:07:52 -0700

    reviews filter

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -12,3 +12,9 @@ for d in data:
 	sum_len = sum_len + len(d)
 
 print('留言的平均長度為', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('總共有', len(new), '筆留言小於100')
